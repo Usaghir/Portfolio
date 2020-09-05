de688161b6b3f8ea9d35738e09b52ce22935cd81
usaghir <umer.saghir@live.com>
2020-09-06 00:03:42 +0200

    Added changes to deploy to production

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 const path = require('path');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === 'production') {
   //server static content.
   // npm run build.
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'front-end/build')));
 }
 app.get('/form', async (req, res) => {
   try {
@@ -76,7 +76,7 @@ app.post('/send', async (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'front-end/build/index.html'));
 });
 
 app.listen(PORT, () => {
