5630346ec7af1b69575be0ae3191b58541d1cfe1
usaghir <umer.saghir@live.com>
2022-11-29 01:43:20 +0100

    change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const PORT =  5000;
+const PORT = process.env.PORT || 5000;
 const path = require('path');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
