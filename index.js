938e851a8f55879b5096ab4e92b20c652b319b8a
usaghir <umer.saghir@live.com>
2023-11-08 16:36:40 +0100

    add build

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const PORT = 5000 ;
-const path = require('path');
-const bodyParser = require('body-parser');
-const exphbs = require('express-handlebars');
-const nodemailer = require('nodemailer');
+const cors = require("cors");
+const PORT = 5000;
+const path = require("path");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+const nodemailer = require("nodemailer");
 
 app.use(cors());
 app.use(express.json()); //req.body
 
 //view engine setup
-app.engine('handlebars', exphbs.engine());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
 
 //Body Parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   //server static content.
   // npm run build.
-  app.use(express.static(path.join(__dirname, 'front-end/build')));
+  app.use(express.static(path.join(__dirname, "front-end/build")));
 }
 
-app.get('/api/form', async (req, res) => {
+app.get("/api/form", async (req, res) => {
   try {
-    res.send('hello');
+    res.send("hello");
   } catch (err) {
     console.error(err.message);
   }
 });
 
-app.post('/api/send', async (req, res) => {
+app.post("/api/send", async (req, res) => {
   try {
     console.log(req.body);
-    res.send('hello');
+    res.send("hello");
 
     const sentData = `
   <h3>Sender Details</h3>
@@ -47,40 +47,44 @@ app.post('/api/send', async (req, res) => {
   <p>${req.body.message}</p>`;
     // create reusable transporter object using the default SMTP transport
     let transporter = await nodemailer.createTransport({
-      host: 'send.one.com',
+      host: "send.one.com",
       port: 587,
       secure: false, // true for 465, false for other ports
       auth: {
-        user: 'contact@rajaumersaghir.com', // generated ethereal user
-        pass: 'Random001', // generated ethereal password
+        user: "contact@rajaumersaghir.com", // generated ethereal user
+        pass: "Random001", // generated ethereal password
       },
     });
 
     // send mail with defined transport object
     let info = await transporter.sendMail({
-      from: '`Email from rajaumersaghir.com account` <contact@rajaumersaghir.com>', // sender address
-      to: 'umer.saghir@live.com', // list of receivers
-      subject: 'New message from rajaumersaghir.com', // Subject line
-      text: 'Hello world?', // plain text body
+      from: "`Email from rajaumersaghir.com account` <contact@rajaumersaghir.com>", // sender address
+      to: "umer.saghir@live.com", // list of receivers
+      subject: "New message from rajaumersaghir.com", // Subject line
+      text: "Hello world?", // plain text body
       html: `<b>${sentData}</b>`, // html body
     });
 
-    console.log('Message sent: %s', info.messageId);
+    console.log("Message sent: %s", info.messageId);
     // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
     // Preview only available when sending through an Ethereal account
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   } catch (err) {
     console.error(err.message);
   }
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'front-end/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "front-end/build/index.html"),
+    function (err) {
+      res.status(500).send(err);
+    }
+  );
 });
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`server has started on port ${PORT}`);
 });
- 
