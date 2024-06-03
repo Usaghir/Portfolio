8d4da45a82aae6f69f748f8bab0f3492fe850da3
usaghir <umer.saghir@live.com>
2024-06-03 12:31:37 +0200

    update email sent

--- back-end/index.js
+++ back-end/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = 5000;
 const path = require("path");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 const nodemailer = require("nodemailer");
+require("dotenv").config(); // Load environment variables from .env file
+
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json()); //req.body
@@ -19,8 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === "production") {
-  //server static content.
-  // npm run build.
+  //serve static content
   app.use(express.static(path.join(__dirname, "front-end/build")));
 }
 
@@ -38,38 +39,35 @@ app.post("/api/send", async (req, res) => {
     res.send("hello");
 
     const sentData = `
-  <h3>Sender Details</h3>
-  <ul>  
-    <li>Name: ${req.body.name}</li>
-    <li>Email: ${req.body.email}</li>
-  </ul>
-  <h3>Message</h3>
-  <p>${req.body.message}</p>`;
+      <h3>Sender Details</h3>
+      <ul>  
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+      </ul>
+      <h3>Message</h3>
+      <p>${req.body.message}</p>`;
+
     // create reusable transporter object using the default SMTP transport
-    let transporter = await nodemailer.createTransport({
-      host: "send.one.com",
-      port: 587,
-      secure: false, // true for 465, false for other ports
+    let transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: true, // true for 465, false for other ports
       auth: {
-        user: "rajaumersaghir@toptechsol.se", // generated ethereal user
-        pass: "Random001", // generated ethereal password
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
     // send mail with defined transport object
     let info = await transporter.sendMail({
-      from: "`Email from rajaumersaghir.com account` <contact@rajaumersaghir.com>", // sender address
-      to: "umer.saghir@live.com", // list of receivers
+      from: `"Email from ${process.env.EMAIL_USER} account" <${process.env.EMAIL_USER}>`, // sender address
+      to: "rajaumersaghir@toptechsol.com", // list of receivers
       subject: "New message from rajaumersaghir.com", // Subject line
       text: "Hello world?", // plain text body
       html: `<b>${sentData}</b>`, // html body
     });
 
     console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   } catch (err) {
     console.error(err.message);
@@ -85,6 +83,6 @@ app.get("*", (req, res) => {
   );
 });
 
-app.listen(process.env.PORT || PORT, () => {
+app.listen(PORT, () => {
   console.log(`server has started on port ${PORT}`);
 });
